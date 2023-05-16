23e17db020b47247199bcd401de72066a43ba69b
nlok5923 <nlokhande5923@gmail.com>
2023-05-16 19:35:18 +0530

    updatd tutorial with latest sdk

--- src/App.js
+++ src/App.js
@@ -1,142 +1,127 @@
-import './App.css';
-import { Banana } from '@rize-labs/banana-wallet-sdk/dist/BananaProvider'
-import { Chains } from '@rize-labs/banana-wallet-sdk/dist/Constants';
-import { useEffect, useState } from 'react';
-import GreeterArtifact from './Greeter.json'
-import { ethers } from 'ethers';
+import "./App.css";
+import { Banana, Chains } from "@rize-labs/banana-wallet-sdk";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { SampleAbi } from "./SampleAbi";
 
 function App() {
-
   useEffect(() => {
     getBananaInstance();
   }, []);
 
-  const [walletName, setWalletName] = useState('');
-  const [walletAddress, setWalletAddress] = useState('')
+  const [walletAddress, setWalletAddress] = useState("");
   const [bananaSdkInstance, setBananSdkInstance] = useState(null);
-  const [signedMessage, setSignedMessage] = useState('')
-  const [signature, setSignature] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [messageTobeSigned, setMessageTobeSigned] = useState('');
+  const [walletInstance, setWalletInstance] = useState(null);
+  const [output, setOutput] = useState("");
+
+  const SampleContractAddress = "0xCB8a3Ca479aa171aE895A5D2215A9115D261A566";
 
   const getBananaInstance = () => {
-    const jsonRpcMumbaiUrl = 'https://polygon-mumbai.g.alchemy.com/v2/cNkdRWeB8oylSQJSA2V3Xev2PYh5YGr4'
-    const bananaInstance = new Banana(Chains.mumbai, jsonRpcMumbaiUrl);
-    setBananSdkInstance(bananaInstance)
-  }
+    const bananaInstance = new Banana(Chains.chiadoTestnet);
+    setBananSdkInstance(bananaInstance);
+  };
 
   const createWallet = async () => {
-    if(walletName === "") {
-      alert("Wallet name can't be empty!!")
-      return;
-    }
     setIsLoading(true);
-    const isWalletNameUnqiue = await bananaSdkInstance.isWalletNameUnique(walletName);
-    if(!isWalletNameUnqiue) {
-      alert("Wallet name provided is not unique");
-      setIsLoading(false);
-      return
-    }
-    const walletAddres = (await bananaSdkInstance.createWallet(walletName)).address
-    console.log(walletAddres);
-    setWalletAddress(walletAddres)
+    const wallet = await bananaSdkInstance.createWallet();
+    setWalletInstance(wallet);
+    const address = await wallet.getAddress();
+    setWalletAddress(address);
+    setOutput("Wallet Address: " + address);
     setIsLoading(false);
-  }
+  };
 
   const connectWallet = async () => {
     const walletName = bananaSdkInstance.getWalletName();
-    if(walletName) {
-      setIsLoading(true)
-      const walletAddress = (await bananaSdkInstance.connectWallet(walletName)).address
-      setWalletAddress(walletAddress)
+    if (walletName) {
+      setIsLoading(true);
+      const wallet = await bananaSdkInstance.connectWallet(walletName);
+      setWalletInstance(wallet);
+      const address = await wallet.getAddress();
+      setWalletAddress(address);
+      setOutput("Wallet Address: " + address);
       setIsLoading(false);
     } else {
       setIsLoading(false);
       alert("You don't have wallet created!");
     }
-  }
+  };
 
   const makeTransaction = async () => {
+    setIsLoading(true);
+    const signer = walletInstance.getSigner();
+    const amount = "0.00001";
+    const tx = {
+      gasLimit: "0x55555",
+      to: SampleContractAddress,
+      value: ethers.utils.parseEther(amount),
+      data: new ethers.utils.Interface(SampleAbi).encodeFunctionData(
+        "stake",
+        []
+      ),
+    };
 
-    let bananaProvider = await bananaSdkInstance.getBananaProvider();
-    let bananaSigner = bananaProvider.getSigner();
-    const greeterAddress = "0x454d3A39dFf28E15b82DE77122e3b0beb461CF22";
-
-    let greeterContract = new ethers.Contract(
-      greeterAddress,
-      GreeterArtifact.abi,
-      bananaSigner
-    );
-
-    const updateValueCallData = greeterContract.interface.encodeFunctionData("updateValue", ["10"]);
     try {
-      setIsLoading(true);
-        const txn = await bananaSdkInstance.execute(
-            updateValueCallData,
-            greeterAddress,
-            "0"
-        );    
+      const txn = signer.sendTransaction(tx);
+      setOutput(JSON.stringify(txn));
     } catch (err) {
-      setIsLoading(false);
-        console.log(err);
+      console.log(err);
     }
     setIsLoading(false);
-  }
+  };
 
   const signMessage = async () => {
-    setIsLoading(true)
-    const sampleMsg = "Hello World"; 
-    const eoaAddress = await bananaSdkInstance.getEOAAddress();
-    const signMessageResponse = await bananaSdkInstance.signMessage(sampleMsg);
-    const messageSigned = signMessageResponse.messageSigned;
-    const signature = signMessageResponse.signature;
-    console.log(messageSigned, signature, eoaAddress);
-    const isVerified = await bananaSdkInstance.verifySignature(signature, messageSigned, eoaAddress);
-    console.log(isVerified);
-    setIsLoading(false)
-  }
+    setIsLoading(true);
+    const sampleMsg = "Hello World";
+    const signer = walletInstance.getSigner();
+    const signMessageResponse = await signer.signBananaMessage(sampleMsg);
+    setOutput(JSON.stringify(signMessageResponse));
+    setIsLoading(false);
+  };
 
-  const test = async () => {
+  const getChainId = async () => {
+    setIsLoading(true);
+    const signer = walletInstance.getSigner();
+    const chainid = signer.getChainId();
+    setOutput(JSON.stringify(chainid));
+    setIsLoading(false);
+  };
 
-    const jsonRpcProviderUrl = 'https://rpc.ankr.com/eth_goerli'
-    const bananaInstanceTemp = new Banana(Chains.goerli, jsonRpcProviderUrl);
-
-    console.log('bananaInstanceTemp', bananaInstanceTemp)
-
-    await bananaInstanceTemp.resetWallet();
-
-    const walletNameTemp = 'test-asluddfdfh87234szabc';
-    try {
-      await bananaInstanceTemp.connectWallet(walletNameTemp)
-    } catch (err) {
-      console.log(err)
-    }
-
-    console.log('walletNameTemp', walletNameTemp)
-
-    const isWalletNameUniqueTemp = await bananaInstanceTemp.isWalletNameUnique(walletNameTemp)
-    console.log('isWalletNameUniqueTemp', isWalletNameUniqueTemp)
-
-    const walletAddressTemp = await bananaInstanceTemp.getEOAAddress(walletNameTemp)
-    console.log('walletAddressTemp', walletAddressTemp)
-
-    const bananaProvider = await bananaInstanceTemp.getBananaProvider()
-    console.log('bananaProvider', bananaProvider)
-  }
+  const getNetwork = async () => {
+    setIsLoading(true);
+    const provider = walletInstance.getProvider();
+    const network = provider.getNetwork();
+    setOutput(JSON.stringify(network));
+    setIsLoading(false);
+  };
 
   return (
     <div className="App">
-      {isLoading && <p> Loading...</p>}
-      <input className='input' type="text" placeholder="Enter wallet name" onChange={(e) => setWalletName(e.target.value)}></input> 
+      <h1>Banana SDK Demo</h1>
       {walletAddress && <p> Wallet Address: {walletAddress}</p>}
-      <button className='btn' onClick={() => createWallet()}> Create Wallet </button> <br />
-      <button className='btn' onClick={() => connectWallet()}> Connect Wallet </button> <br />
-      <button className='btn' onClick={() => makeTransaction()}> Make transaction  </button> <br />
-      <input className='input' type="text" placeholder="Enter message to be signed" onChange={(e) => setMessageTobeSigned(e.target.value)}></input> <br />
-      <button className='btn' onClick={() => signMessage()}> Sign message  </button>
-      {signedMessage && <p>Signed message: {signedMessage} </p>}
-      {signature && <p> Signature: {signature} </p>}
-      <button className='btn'onClick={() => test()}  > test </button>
+      <button className="btn" onClick={() => createWallet()}>
+        Create Wallet
+      </button>
+      <button className="btn" onClick={() => connectWallet()}>
+        Connect Wallet
+      </button>
+      <button className="btn" onClick={() => getChainId()}>
+        ChainId
+      </button>
+      <button className="btn" onClick={() => getNetwork()}>
+        Network
+      </button>
+      <button className="btn" onClick={() => makeTransaction()}>
+        Make transaction
+      </button>
+      <button className="btn" onClick={() => signMessage()}>
+        Sign message
+      </button>
+      <h1> Output Panel</h1>
+      <div className="output-div">
+        <p>{isLoading ? "Loading.." : output}</p>
+      </div>
     </div>
   );
 }
